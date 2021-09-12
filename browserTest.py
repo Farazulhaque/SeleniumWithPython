ad7f1760f2acc167c9f6ffa851981f35bd8a451a
Faraz <farazattari786@gmail.com>
2021-09-12 13:10:27 +0530

    get current url, page source

--- browserTest.py
+++ browserTest.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 # Location of chrome web driver stored in system
-driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")             # Chrome
+# driver = webdriver.Firefox(executable_path="C:\Drivers\geckodriver-v0.29.1\geckodriver.exe")          # Firefox
+# driver = webdriver.Ie(executable_path="C:\Drivers\IEDriverServer_Win32_3.150.2\IEDriverServer.exe")   # Internet
+# Explorer driver = webdriver.Edge(executable_path="C:\Drivers\edgedriver_win64\msedgedriver.exe        #Microsoft Edge
+
 
 # Open URL
-driver.get("https://python.org/")
+driver.get("https://farazulhaque.github.io/weatherappproject/")
 
 # print title of web page
 print(driver.title)
+
+# print current url
+print(driver.current_url)
+
+# get page source
+print(driver.page_source)
 
 # close browser
 driver.close()
